f3bb9022c6763e4304d20fd41283d59c44ea4a5f
Brian Hanratty <bhanratt@asu.edu>
2017-11-02 13:00:11 -0700

    Add files via upload

--- GetGeneIDs.py
+++ GetGeneIDs.py
@@ -6,31 +6,37 @@
 import sys
 data_table = {}
 
+try:
+    gene=sys.argv[1].upper()
+except Exception:
+    print("You need to specify a gene. e.g. \"python GetGeneIDs.py tp53\"")
+    sys.exit(1)   
+print("Using gene: "+gene)
+
 #Import Gene Dictionary
 try:
-	f1=open('AllGeneIDs.txt')
-	for line in f1:
-	    geneid = str.strip(line.split('|')[0])
-	    genesymbol = str.strip(line.split('|')[1])
-	    data_table.setdefault(genesymbol, set( )).add(geneid)
+    f1=open('AllGeneIDs.txt')
+    for line in f1:
+        geneid = str.strip(line.split('|')[0])
+        genesymbol = str.strip(line.split('|')[1])
+        data_table.setdefault(genesymbol, set( )).add(geneid)
 
 #Missing dictionary handling
 except FileNotFoundError:
-	print("Missing dependency: AllGeneIDs.txt")
-	sys.exit(1)
+    print("Missing dependency: AllGeneIDs.txt")
+    sys.exit(1)
 
-gene=raw_input('Enter a gene symbol: ').upper()
 
 #Do the thing
 if gene in data_table:
-	matchedids=list(data_table[gene])
-	resultline= ' -e '.join(matchedids)
-	fw=open('grepline.txt', 'w')
-	fw.write('grep -e '+resultline+' $i.blr > $i.blr.tp53')
-	fw.close()
+    matchedids=list(data_table[gene])
+    resultline= ' -e '.join(matchedids)
+    fw=open('grepline.txt', 'w')
+    fw.write('grep -e '+resultline+' $i.blr > $i.blr.tp53')
+    fw.close()
 else:
-	print("Gene not found in dictionary. Maybe a typo?")
-	sys.exit(1)
+     print(gene+" not found in dictionary. Maybe a typo?")
+     sys.exit(1)
 
 print('All done!')
 f1.close()
