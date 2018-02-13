f7e8ca3f5f4a1cad0d53264f21f56a461fcf9ca7
Brian Hanratty <bhanratt@asu.edu>
2018-02-13 09:38:18 -0700

    Update automate.sh
    Added functionality to place results in a results directory and gene-specific subdirectories containing each result by by species along with a concatenated result file for all species.

--- automate.sh
+++ automate.sh
@@ -4,16 +4,27 @@
 #Automation script for looping through all reference genomes in ref/ and all proteins in prot/
 #Important: All reference genomes and proteins must be in .fa format and loaded into ref/ and prot/ respectively
 #Created: 11/2/2017
-#Modified: 11/2/2017
+#Modified: 2/9/2018
+mkdir results
 for reference in ref/*.fa
 	do
 		for protein in prot/*.fa
 			do
 			prot=`echo ${protein%.fa} | sed 's,^[^/]*/,,'`
 			ref=`echo ${reference%.fa} | sed 's,^[^/]*/,,'`
+			mkdir results/$prot
 			python GetGeneIDs.py $prot
 			bash logcollector.sh $reference $protein
-			mv Results.fa Results.$ref.$prot.fa
-			mv Results.blr Results.$ref.$prot.blr
+			mv Results.fa results/$prot/Results.$prot.$ref.fa
+			mv Results.blr results/$prot/Results.$prot.$ref.blr
 		done
 	done
+cd results
+for dir in *
+	do
+		cd $dir
+		cat *.fa > Results.$dir.all.fa
+		cat *.blr > Results.$dir.all.blr
+		cd ..
+	done
+cd ..
