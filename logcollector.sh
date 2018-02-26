73fc6cc50fe2fee5dc3dacb71b868174c9993a66
Brian Hanratty <bhanratt@asu.edu>
2018-02-26 16:20:17 -0700

    Added filtering capability for sequence length

--- logcollector.sh
+++ logcollector.sh
@@ -8,24 +8,34 @@
 #Dependencies: blat, blastx, pslToBed, bedtools
 #Includes: split.pl
 
-if [ $# -lt 2 ]; then
-	echo "Usage: $0 <genome fasta> <protein fasta>"
+if [ $# -lt 3 ]; then
+	echo "Usage: $0 <genome fasta> <protein fasta> <y/n for length filter>"
 	exit 1
 fi
 
 genome=$1
 peptide=$2
+lengthfilter=$3
+peplength=`cat $peptide | awk '$0 ~ ">" {print c; c=0; } $0 !~ ">" {c+=length($0);} END { print c; }' | tail -n 1`
+p=$((3*peplength))
 genomename=`echo ${1%.fa} | sed 's,^[^/]*/,,'`
 peptidename=`echo ${2%.fa} | sed 's,^[^/]*/,,'`
 grepline=`cat grepline.txt`
 
-echo 'Ortholog detector by ASU Bioinformatics Core Lab (asubioinformatics.org) - Last Updated: 2/9/2018'
+echo 'Ortholog detector by ASU Bioinformatics Core Lab (asubioinformatics.org) - Last Updated: 2/26/2018'
 echo 'using genome ' $genomename;
 echo 'using peptide ' $peptidename;
+echo 'peptide length: ' $peplength;
+if [ "$lengthfilter" = "y" ]; then
+	echo 'Length filter ON. Will remove any sequences smaller than: '$p;
+else
+	echo 'Length filter OFF.';
+fi
+
 date
 echo 'Running Step 1: blat';
+
 #can set -minScore=65 for more specific results
-#maybe implement pblat to speed up the blat step https://github.com/icebert/pblat
 tools/blat $1 $2 -t=dnax -q=prot -minIdentity=65 -minScore=55 $genomename.$peptidename.blat.psl;
 date
 echo 'Running Step 2: pslToBed';
@@ -33,17 +43,34 @@ tools/pslToBed $genomename.$peptidename.blat.psl $genomename.$peptidename.blat.p
 date
 echo 'Running Step 3: bedtools';
 bedtools getfasta -s -split -fi $genome -bed $genomename.$peptidename.blat.psl.bed -fo Copies.$peptidename;
+
 #split fasta by IDs
 perl tools/split.pl --input_file Copies.$peptidename --output_dir ./;
 date
 echo 'Running Step 4: blastx and parse for protein';
+
+#logic to filter out sequences that are smaller than 3X peptide length
+if [ "$lengthfilter" = "y" ]; then
+	for i in *.split
+		do
+		size=`cat $i | awk '$0 ~ ">" {print c; c=0; } $0 !~ ">" {c+=length($0);} END { print c; }' | tail -n 1`
+		if [ "$size" -ge "$p" ]; then
+			echo 'Passed the length filter: '$i;
+		else
+			echo 'Failed the length filter: '$i;
+			rm $i;
+		fi
+		done
+fi
 #parse split protein fastas for occurences of TP53 IDs and filter by identity >=65%
 for i in *.split
-do
+	do
 	blastx -outfmt 6 -query $i -subject tools/humanprotein.fa > $i.blr;
 	eval $grepline
 	awk '$3 >= 65.00' $i.blr.tp53 > $i.blr.tp53.passed;
-done
+	done
+
+
 #remove non-matches
 find . -name '*.passed' -size 0 -delete
 #save potential hits
